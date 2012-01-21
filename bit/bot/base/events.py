e07cf96d3e81695560d9c4c619d939d6e7aa7d53
Ryan Northey <ryan@3ca.org.uk>
2012-01-21 01:27:40 +0000

    base

---
+++ bit/bot/base/events.py
@@ -0,0 +1,22 @@
+
+
+
+class BotRespondsEvent(object):
+    def __init__(self,bot):
+        self.bot = bot
+
+    def update(self,sessionID,response):
+        self.session_id = sessionID
+        self.message = response
+        return self
+
+
+class PersonSpeaksEvent(object):
+    def __init__(self,bot):
+        self.bot = bot
+
+    def update(self,sessionID,response):
+        self.session_id = sessionID
+        self.message = response
+        return self
+
