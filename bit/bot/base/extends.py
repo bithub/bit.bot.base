d56a0766e548128387c6bb9707849188e7f5d4d0
Ryan Northey <ryan@3ca.org.uk>
2012-01-29 12:15:14 +0000

    making plugins extensible

---
+++ bit/bot/base/extends.py
@@ -0,0 +1,43 @@
+
+from zope.interface import implements
+from zope.component import getUtility, provideUtility, provideHandler,provideAdapter, queryUtility
+
+from bit.core.interfaces import IPlugin, IServices, IPluginExtender
+
+class SocketsPlugin(object):
+    implements(IPluginExtender)
+    def __init__(self,plugin):
+        self.plugin = plugin
+
+    def extend(self):
+        if hasattr(self.plugin,'load_sockets'):
+            getattr(self.plugin,'load_sockets')()      
+
+
+class AgentsPlugin(object):
+    implements(IPluginExtender)
+    def __init__(self,plugin):
+        self.plugin = plugin
+        
+    def extend(self):
+        if hasattr(self.plugin,'load_agents'):
+            getattr(self.plugin,'load_agents')()      
+        else:
+            agents = getattr(self.plugin,'_agents',{})
+            [getUtility(IAgents).add(self.plugin.name,name,*agent)
+             for name,agent in agents.items()]
+            
+class HandlersPlugin(object):
+   implements(IPluginExtender)
+   def __init__(self,plugin):
+      self.plugin = plugin
+
+   def extend(self):
+       if hasattr(self.plugin,'load_handlers'):
+           getattr(self.plugin,'load_handlers')()      
+           
+       else:
+           handlers = getattr(self.plugin,'_handlers',[])
+           for handler in handlers:
+               provideHandler(handler)
+               
--- bit/bot/base/plugin.py
+++ bit/bot/base/plugin.py
@@ -12,8 +12,7 @@ zope.component.event
 from twisted.application.service import IServiceCollection
 from twisted.web import static
 
-
-from bit.core.interfaces import IPlugin, IServices
+from bit.core.interfaces import IPlugin, IServices, IPluginExtender
 from bit.core.plugin import BitPlugin
 
 from bit.bot.common.interfaces import IIntelligent, IWebImages, IWebCSS, IWebJS, IWebHTML, IWebJPlates, IFlatten, IAgents, ISubscriptions
@@ -22,11 +21,14 @@ from bit.bot.base.subscriptions import Subscriptions
 from bit.bot.base.handlers import  rubbish_collection
 from bit.bot.base.agent import AgentRubbish, Agents, AgentsFlattener
 from bit.bot.base.services import ServicesFlattener, Services
+from bit.bot.base.extends import HandlersPlugin, AgentsPlugin, SocketsPlugin
 
 from bit.bot.common.interfaces import IHTTPRoot
 
 class BotPlugin(BitPlugin):
     implements(IPlugin)
+
+    # remove these!
     _services = {}
     _handlers = []
     _services = {}
@@ -34,35 +36,6 @@ class BotPlugin(BitPlugin):
     _aiml = []
     _agents = {}
     _utils = []
-
-    def load_agents(self):
-        [getUtility(IAgents).add(self.name,name,*agent)
-         for name,agent in self._agents.items()]
-
-    def load_handlers(self):
-        for handler in self._handlers:
-            provideHandler(handler)
-
-    def load_HTTP(self):
-        fpath =  os.path.dirname(inspect.getfile(self.__class__))
-        for hid,http in self._http.items():
-            if hid == 'root':
-                target = os.path.join(fpath,http)
-                for rtype in os.listdir(target):
-                    resource = queryUtility(IHTTPRoot,rtype)
-                    if not resource: continue
-                    resource.add_resources(os.path.join(target,rtype))
-    
-    def load_AIML(self):
-        fpath =  os.path.dirname(inspect.getfile(self.__class__))
-        for aiml in self._aiml:
-            target = os.path.join(fpath,aiml)
-            if os.path.isdir(target):
-                for f in os.listdir(target):
-                    if f.endswith('.aiml'):
-                        getUtility(IIntelligent).learn(os.path.join(target,f))
-            elif os.path.isfile(target):
-                getUtility(IIntelligent).learn(target)
 
 class BitBot(BotPlugin):
     implements(IPlugin)
@@ -84,4 +57,6 @@ class BitBot(BotPlugin):
         provideAdapter(ServicesFlattener,[IServices,],IFlatten)      
   
 
-
+        provideAdapter(SocketsPlugin,[IPlugin,],IPluginExtender)        
+        provideAdapter(AgentsPlugin,[IPlugin,],IPluginExtender)        
+        provideAdapter(HandlersPlugin,[IPlugin,],IPluginExtender)        
