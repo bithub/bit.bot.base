3d20dbebd4c580395da87ace07323d5612db3d82
Ryan Northey <ryan@3ca.org.uk>
2012-03-22 13:49:54 +0000

    factoring out configuration loader, and adding param to twistd to set config file

--- bit/bot/base/tap.py
+++ bit/bot/base/tap.py
@@ -6,20 +6,16 @@ zope.component.event
 
 from twisted.python import usage
 
-from bit.core.configuration import Configuration, FileConfiguration
+from bit.core.configuration import ConfigurationLoader
 from bit.core.interfaces import IConfiguration, IApplicationRunner
 
 
 class Options(usage.Options):
-    pass
+
+    optParameters = [
+        ['config', 'c', 'bit-bot.cfg'],
+        ]
 
 
 def makeService(config):
-    configuration = Configuration()
-    file_configuration = FileConfiguration('curate.cfg')
-    provideUtility(configuration, IConfiguration)
-    provideUtility(file_configuration, IConfiguration, name='default')
-    for extension in [x.strip() for x in configuration.get('bot', 'extends')]:
-        provideUtility(FileConfiguration(extension),
-                       IConfiguration, name=os.path.basename(extension))
-    return IApplicationRunner(configuration).service
+    return IApplicationRunner(ConfigurationLoader(config).load()).service
